ff8d3b7e867d2ea8e462717081d30be252046228
SimonEntholzer <nom.ise@outlook.com>
2021-01-25 12:57:07 +0100

    adapted tests

--- src/test/javascript/spec/component/file-upload-assessment-dashboard/file-upload-assessment-dashboard.component.spec.ts
+++ src/test/javascript/spec/component/file-upload-assessment-dashboard/file-upload-assessment-dashboard.component.spec.ts
@@ -16,23 +16,21 @@ import { ExerciseType } from 'app/entities/exercise.model';
 import { ExerciseService } from 'app/exercises/shared/exercise/exercise.service';
 import { CourseManagementService } from 'app/course/manage/course-management.service';
 import { HttpResponse } from '@angular/common/http';
-import { ModelingSubmissionService } from 'app/exercises/modeling/participate/modeling-submission.service';
 import { ModelingExercise } from 'app/entities/modeling-exercise.model';
 import { AssessmentType } from 'app/entities/assessment-type.model';
-import { ModelingAssessmentService } from 'app/exercises/modeling/assess/modeling-assessment.service';
 import { SortService } from 'app/shared/service/sort.service';
 import { FileUploadSubmissionService } from 'app/exercises/file-upload/participate/file-upload-submission.service';
 import { FileUploadAssessmentsService } from 'app/exercises/file-upload/assess/file-upload-assessment.service';
+import { FileUploadExercise } from 'app/entities/file-upload-exercise.model';
 
 const route = { params: of({ courseId: 3, exerciseId: 22 }) };
 const course = { id: 1 };
-const fileUploadExercise = {
+let fileUploadExercise: FileUploadExercise;
+fileUploadExercise = {
     id: 22,
-    course_id: course.id,
     type: ExerciseType.FILE_UPLOAD,
-    assessmentType: AssessmentType.SEMI_AUTOMATIC,
-    numberOfAssessmentsOfCorrectionRounds: [],
-};
+} as FileUploadExercise;
+
 const fileUploadSubmission1 = { id: 1, submitted: true, results: [{ id: 10, assessor: { id: 20, guidedTourSettings: [] } }] };
 const fileUploadSubmission2 = { id: 2, submitted: true, results: [{ id: 20, assessor: { id: 30, guidedTourSettings: [] } }] };
 const userId = 30;
@@ -86,249 +84,77 @@ describe('ModelingAssessmentDashboardComponent', () => {
 
     it('should set parameters and call functions on init', fakeAsync(() => {
         // setup
-        const getSubmissionsSpy = spyOn(component, 'getSubmissions');
-        const registerChangeInResultsSpy = spyOn(component, 'registerChangeInResults');
-
+        spyOn<any>(component, 'getSubmissions');
+        spyOn<any>(component, 'setPermissions');
         // test for init values
         expect(component).toBeTruthy();
         expect(component.submissions).toEqual([]);
         expect(component.reverse).toEqual(false);
         expect(component.predicate).toEqual('id');
         expect(component.filteredSubmissions).toEqual([]);
-        expect(component.optimalSubmissions).toEqual([]);
-        expect(component.otherSubmissions).toEqual([]);
 
         // call
         component.ngOnInit();
         tick();
 
         // check
-        expect(getSubmissionsSpy).toHaveBeenCalledWith(true);
-        expect(registerChangeInResultsSpy).toHaveBeenCalled();
+        expect(component['getSubmissions']).toHaveBeenCalledWith(fileUploadExercise.id);
+        expect(component['setPermissions']).toHaveBeenCalled();
         expect(courseFindSpy).toHaveBeenCalled();
         expect(exerciseFindSpy).toHaveBeenCalled();
-        expect(component.course).toEqual(course);
-        expect(component.modelingExercise).toEqual(modelingExercise as ModelingExercise);
+        expect(component.exercise).toEqual(fileUploadExercise as FileUploadExercise);
     }));
 
     it('should get Submissions', () => {
         // test getSubmissions
-        const filterSubmissionsSpy = spyOn(component, 'filterSubmissions');
-        const modelingSubmissionServiceSpy = spyOn(modelingSubmissionService, 'getModelingSubmissionsForExerciseByCorrectionRound').and.returnValue(
-            of(new HttpResponse({ body: [modelingSubmission] })),
+        const fileUploadSubmissionServiceSpy = spyOn(fileUploadSubmissionService, 'getFileUploadSubmissionsForExerciseByCorrectionRound').and.returnValue(
+            of(new HttpResponse({ body: [fileUploadSubmission1] })),
         );
 
         // call
         component.ngOnInit();
 
         // check
-        expect(modelingSubmissionServiceSpy).toHaveBeenCalledWith(modelingExercise.id, { submittedOnly: true });
-        expect(component.submissions).toEqual([modelingSubmission]);
-        expect(component.filteredSubmissions).toEqual([modelingSubmission]);
-        expect(filterSubmissionsSpy).toHaveBeenCalled();
+        expect(fileUploadSubmissionServiceSpy).toHaveBeenCalledWith(fileUploadExercise.id, { submittedOnly: true });
+        expect(component.submissions).toEqual([fileUploadSubmission1]);
+        expect(component.filteredSubmissions).toEqual([fileUploadSubmission1]);
     });
 
     it('should update filtered submissions', () => {
         // test updateFilteredSubmissions
-        const applyFilter = spyOn(component, 'applyFilter');
 
         // setup
         component.ngOnInit();
-        component.updateFilteredSubmissions([modelingSubmission]);
+        component.updateFilteredSubmissions([fileUploadSubmission1]);
 
         // check
-        expect(component.filteredSubmissions).toEqual([modelingSubmission]);
-        expect(applyFilter).toHaveBeenCalled();
-    });
-
-    it('should refresh', () => {
-        // test refresh
-        const getSubmissionsSpy = spyOn(component, 'getSubmissions');
-
-        component.refresh();
-
-        expect(getSubmissionsSpy).toHaveBeenCalledWith(true);
-    });
-
-    describe('filter Submissions', () => {
-        it('should filter Submissions', () => {
-            // test filterSubmissions
-            // setup
-            const applyFilter = spyOn(component, 'applyFilter');
-            const getOptimalSubmissionsSpy = spyOn(modelingAssessmentService, 'getOptimalSubmissions').and.returnValue(of([1]));
-            component.modelingExercise = modelingExercise;
-            component.nextOptimalSubmissionIds = [];
-
-            // call
-            component.filterSubmissions(true);
-
-            // check
-            expect(getOptimalSubmissionsSpy).toHaveBeenCalled();
-            expect(component.nextOptimalSubmissionIds).toEqual([1]);
-            expect(applyFilter).toHaveBeenCalled();
-        });
-        it('should not filter Submissions', () => {
-            // test filterSubmissions
-            // setup
-            const applyFilter = spyOn(component, 'applyFilter');
-            const getOptimalSubmissionsSpy = spyOn(modelingAssessmentService, 'getOptimalSubmissions').and.returnValue(of([1]));
-
-            component.modelingExercise = modelingExercise;
-            component.modelingExercise.assessmentType = AssessmentType.AUTOMATIC;
-            component.nextOptimalSubmissionIds = [];
-
-            // call
-            component.filterSubmissions(true);
-
-            // check
-            expect(getOptimalSubmissionsSpy).not.toHaveBeenCalled();
-            expect(component.nextOptimalSubmissionIds).toEqual([]);
-            expect(applyFilter).toHaveBeenCalled();
-        });
-    });
-
-    it('should apply filters ', () => {
-        // test applyFilters
-        // setup
-        component.submissions = [modelingSubmission, modelingSubmission2];
-        component.userId = userId;
-        component.nextOptimalSubmissionIds = [1, 2];
-        component.filteredSubmissions = component.submissions;
-        // call
-        component.applyFilter();
-
-        // check
-        expect(component.otherSubmissions.length).toBe(1);
-        expect(component.optimalSubmissions.length).toBe(1);
-        expect(component.optimalSubmissions[0].id).toEqual(modelingSubmission2.id);
-        expect(component.otherSubmissions[0].id).toEqual(modelingSubmission.id);
-    });
-    describe('reset optimality', () => {
-        it('should reset optimality', fakeAsync(() => {
-            component.modelingExercise.assessmentType = AssessmentType.SEMI_AUTOMATIC;
-            component.modelingExercise = modelingExercise;
-            const serviceResetOptSpy = spyOn(modelingAssessmentService, 'resetOptimality').and.returnValue(of(1));
-            const filterSubmissionsSpy = spyOn(component, 'filterSubmissions');
-
-            // call
-            component.resetOptimality();
-
-            tick();
-
-            expect(serviceResetOptSpy).toHaveBeenCalledWith(modelingExercise.id);
-            expect(filterSubmissionsSpy).toHaveBeenCalledWith(true);
-        }));
-
-        it('should not reset optimality', () => {
-            // setup
-            component.modelingExercise.assessmentType = AssessmentType.AUTOMATIC;
-            const filterSubmissionsSpy = spyOn(component, 'filterSubmissions');
-
-            // call
-            component.resetOptimality();
-
-            // check
-            expect(filterSubmissionsSpy).not.toHaveBeenCalled();
-        });
-    });
-
-    describe('makeAllSubmissionsVisible', () => {
-        it('should make all submissions visible ', () => {
-            // setup
-            component.busy = false;
-            component.allSubmissionsVisible = false;
-            // call
-            component.makeAllSubmissionsVisible();
-            // check
-            expect(component.allSubmissionsVisible).toBe(true);
-        });
-
-        it('should not make all submissions visible ', () => {
-            // setup
-            component.busy = true;
-            component.allSubmissionsVisible = false;
-            // call
-            component.makeAllSubmissionsVisible();
-            // check
-            expect(component.allSubmissionsVisible).toBe(false);
-        });
-    });
-
-    describe('assessNextOptimal', () => {
-        it('should assess next optimal submission', () => {
-            // setup
-            component.nextOptimalSubmissionIds = [];
-            component.busy = true;
-            const routerNavigateSpy = spyOn(router, 'navigate');
-            const serviceResetOptSpy = spyOn(modelingAssessmentService, 'getOptimalSubmissions').and.returnValue(of([1]));
-            const navigateToNextSpy = spyOn<any>(component, 'navigateToNextRandomOptimalSubmission').and.callThrough(); // <any> bc of private method
-            component.modelingExercise = modelingExercise;
-
-            // call
-            component.assessNextOptimal();
-
-            // check
-            expect(serviceResetOptSpy).toHaveBeenCalledWith(modelingExercise.id);
-            expect(component.busy).toBe(false);
-            expect(navigateToNextSpy).toHaveBeenCalled();
-            expect(routerNavigateSpy).toHaveBeenCalled();
-        });
-
-        it('should navigate to next random optimal submission', () => {
-            // setup
-            component.nextOptimalSubmissionIds = [1];
-            component.busy = false;
-            const routerNavigateSpy = spyOn(router, 'navigate');
-            const navigateToNextSpy = spyOn<any>(component, 'navigateToNextRandomOptimalSubmission').and.callThrough(); // <any> bc of private method
-
-            // call
-            component.assessNextOptimal();
-
-            // check
-            expect(component.busy).toBe(true);
-            expect(navigateToNextSpy).toHaveBeenCalled();
-            expect(routerNavigateSpy).toHaveBeenCalled();
-        });
+        expect(component.filteredSubmissions).toEqual([fileUploadSubmission1]);
     });
 
     it('should cancelAssessment', fakeAsync(() => {
         // test cancelAssessment
         const windowSpy = spyOn(window, 'confirm').and.returnValue(true);
-        const refreshSpy = spyOn(component, 'refresh');
-
-        const modelAssServiceCancelAssSpy = spyOn(modelingAssessmentService, 'cancelAssessment').and.returnValue(of(1));
+        spyOn<any>(component, 'getSubmissions');
+        const modelAssServiceCancelAssSpy = spyOn(fileUploadAssessmentsService, 'cancelAssessment').and.returnValue(of(1));
 
         // call
-        component.cancelAssessment(modelingSubmission);
+        component.cancelAssessment(fileUploadSubmission1);
         tick();
 
         // check
-        expect(modelAssServiceCancelAssSpy).toHaveBeenCalledWith(modelingSubmission.id);
+        expect(modelAssServiceCancelAssSpy).toHaveBeenCalledWith(fileUploadSubmission1.id);
         expect(windowSpy).toHaveBeenCalled();
-        expect(refreshSpy).toHaveBeenCalled();
+        expect(component['getSubmissions']).toHaveBeenCalled();
     }));
 
     it('should sortRows', () => {
         // test cancelAssessment
         const sortServiceSpy = spyOn(sortService, 'sortByProperty');
-        component.otherSubmissions = [modelingSubmission];
         component.predicate = 'predicate';
         component.reverse = false;
 
         component.sortRows();
 
-        expect(sortServiceSpy).toHaveBeenCalledWith([modelingSubmission], 'predicate', false);
-    });
-
-    it('ngOnDestroy', () => {
-        // setup
-        component.paramSub = new Subscription();
-        const paramSubSpy = spyOn(component.paramSub, 'unsubscribe');
-
-        // call
-        component.ngOnDestroy();
-
-        // check
-        expect(paramSubSpy).toHaveBeenCalled();
+        expect(sortServiceSpy).toHaveBeenCalledWith([fileUploadSubmission1], 'predicate', false);
     });
 });
